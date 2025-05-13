a9f2cb33087643ebcf1b281ce0335f5b9d281620
carloozed <carlo_ettisberger@hotmail.com>
2025-05-13 10:06:13 +0200

    hamburger animation

--- src/app/components/Navigation/HeaderContent/Hamburger/Hamburger.tsx
+++ src/app/components/Navigation/HeaderContent/Hamburger/Hamburger.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 import styles from './Hamburger.module.css';
 
 type Props = {};
 
 export default function Hamburger({}: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const clickFunction = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className={styles.hamburger__container}>
+    <div
+      className={`${styles.hamburger__container} ${isOpen && styles.hamburger__open}`}
+      onClick={clickFunction}
+    >
       <div className={styles.hamburger__line}>
         <div className={styles.circle}></div>
         <div className={styles.line}></div>
