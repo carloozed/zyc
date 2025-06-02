30a4de817b50cdcf4e43d63f71a7fc5174d402d4
carloozed <carlo_ettisberger@hotmail.com>
2025-06-02 15:59:21 +0200

    changes

--- src/app/components/TransitionLink/TransitionLink.tsx
+++ src/app/components/TransitionLink/TransitionLink.tsx
@@ -3,10 +3,12 @@
 import React, { useRef } from 'react';
 
 import { asLink, LinkField, PrismicDocument } from '@prismicio/client';
-import { Link } from 'next-view-transitions';
+import Link from 'next/link';
 
 import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
+
+import { useRouter } from 'next/navigation';
 
 gsap.registerPlugin(useGSAP);
 
@@ -23,18 +25,45 @@ export type TransitionLinkProps = {
   | { field?: never; document?: never; href: string }
 );
 
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export function TransitionLink({
   field,
   document: doc,
   href,
   children,
   className,
-  onClick,
   tabIndex,
   hasText = true,
 }: TransitionLinkProps) {
   const url = href ?? asLink(field ?? doc);
   const linkRef = useRef<HTMLAnchorElement>(null);
+
+  const router = useRouter();
+
+  const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const targetUrl = href ?? url;
+
+    // TODO: Run some exit animation here
+
+    const body = document.querySelector('body');
+    body?.classList.add('page-transition');
+
+    // sleep for some time
+
+    await sleep(2500); // Adjust the duration as needed
+    if (typeof targetUrl === 'string') {
+      router.push(targetUrl);
+
+      await sleep(500); // Adjust the duration as needed
+      // TODO: Run some enter animation
+
+      body?.classList.remove('page-transition');
+    }
+  };
 
   if (!url) {
     console.warn('TransitionLink: No valid URL provided');
@@ -46,7 +75,7 @@ export function TransitionLink({
       ref={linkRef}
       href={url}
       className={className}
-      onClick={onClick}
+      onClick={handleTransition}
       tabIndex={tabIndex}
     >
       {hasText ? (field?.text ?? children) : children}
