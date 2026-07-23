64ea3f59f5e1ba372a46e6e2af26e2237914038a
Carlo Ettisberger <105387348+heckspoiler@users.noreply.github.com>
2026-07-23 12:24:47 +0200

    added artistprogramme slice

--- src/slices/ArtistsProgramme/index.tsx
+++ src/slices/ArtistsProgramme/index.tsx
@@ -1,26 +1,49 @@
-import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { FC } from 'react';
+import { Content } from '@prismicio/client';
+import { PrismicRichText, SliceComponentProps } from '@prismicio/react';
 
-/**
- * Props for `ArtistsProgramme`.
- */
-export type ArtistsProgrammeProps = SliceComponentProps<Content.ArtistsProgrammeSlice>;
+import styles from './index.module.css';
 
-/**
- * Component for "ArtistsProgramme" Slices.
- */
+export type ArtistsProgrammeProps =
+  SliceComponentProps<Content.ArtistsProgrammeSlice>;
+
 const ArtistsProgramme: FC<ArtistsProgrammeProps> = ({ slice }) => {
-	return (
-		<section
-			data-slice-type={slice.slice_type}
-			data-slice-variation={slice.variation}
-		>
-			Placeholder component for {slice.slice_type} (variation: {slice.variation}) slices.
-			<br />
-			<strong>You can edit this slice directly in your code editor.</strong>
-		</section>
-	)
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className={styles.wrapper}
+    >
+      <div className={styles.title}>
+        <PrismicRichText field={slice.primary.title} />
+      </div>
+
+      <ul className={styles.categories}>
+        {slice.primary.categories.map((category, index) => (
+          <li
+            key={category.id || index}
+            id={category.id || undefined}
+            className={styles.category}
+          >
+            <PrismicRichText field={category.title} />
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.descriptions}>
+        {slice.primary.descriptions.map((item, index) => (
+          <div
+            key={item.id || index}
+            id={item.id || undefined}
+            className={styles.description}
+          >
+            <PrismicRichText field={item.title} />
+            <PrismicRichText field={item.description} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
-export default ArtistsProgramme
+export default ArtistsProgramme;
